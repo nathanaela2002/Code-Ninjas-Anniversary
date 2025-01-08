6f56ddd63335f028cc53626094cb950abdf60a4b
Ted Lee <tedlee000@hotmail.com>
2025-01-08 16:52:45 -0500

    Installed CSS and set up router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import RiddlePage from "./pages/RiddlePage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
 
 function App() {
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-    <Route path="/riddle:id" element={<RiddlePage />} />
-  </Routes>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/riddle:id" element={<RiddlePage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <div>Home Page</div>
+      <h1 className="text-3xl font-bold underline">Home</h1>
     </div>
   );
 }
